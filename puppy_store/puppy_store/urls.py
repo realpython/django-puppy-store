3e59c8b0efc00eb622800e82c9be74745c397fe3
Kenny <kennyaires@gmail.com>
2023-07-27 01:15:00 -0300

    Updated libs versions

--- puppy_store/puppy_store/urls.py
+++ puppy_store/puppy_store/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('puppies.urls')),
-    url(
+    re_path(r'^', include('puppies.urls')),
+    re_path(
         r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')
     ),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 ]
